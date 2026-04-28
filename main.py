400741393980cebeaa2b607ec3046be2733b46d7
Jay-Tech-tech <arne.jay.leonidas@gmail.com>
2026-04-28 12:26:49 +0200

    UI V0.5

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def on_button_b():
 input.on_button_event(Button.B, input.button_event_click(), on_button_b)
 
 sprite: game.LedSprite = None
+list2: List[number] = []
+X = 0
+Y = 0
+spielfeld = [[0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]]
 sprite = game.create_sprite(2, 0)
 
 def on_every_interval():
@@ -14,7 +22,17 @@ def on_every_interval():
 loops.every_interval(1000, on_every_interval)
 
 def on_forever():
-    # Prüft, ob der Punkt ganz unten angekommen ist (Y-Koordinate ist 4)
-    if sprite.get(LedSpriteProperty.Y) == 4:
-        storage.put_number(StorageSlots.S1, 0)
+    global X, Y
+    X = sprite.get(LedSpriteProperty.X)
+    Y = sprite.get(LedSpriteProperty.Y)
+    if Y == 4:
+        spielfeld[Y][X] = 1
+        game.create_sprite(X, Y)
+        sprite.set(LedSpriteProperty.Y, 0)
+        sprite.set(LedSpriteProperty.X, 2)
+    elif spielfeld[Y + 1][X] == 1:
+        spielfeld[Y][X] = 1
+        game.create_sprite(X, Y)
+        sprite.set(LedSpriteProperty.Y, 0)
+        sprite.set(LedSpriteProperty.X, 2)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -4,17 +4,15 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     sprite.change(LedSpriteProperty.X, 1)
 })
-let Y = 0
-let X = 0
 let sprite: game.LedSprite = null
 let list: number[] = []
-let spielfeld = [
-    [0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0], 
-    [0, 0, 0, 0, 0]  
-]
+let X = 0
+let Y = 0
+let spielfeld = [[0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]]
 sprite = game.createSprite(2, 0)
 loops.everyInterval(1000, function () {
     sprite.change(LedSpriteProperty.Y, 1)
@@ -24,16 +22,13 @@ basic.forever(function () {
     Y = sprite.get(LedSpriteProperty.Y)
     if (Y == 4) {
         spielfeld[Y][X] = 1
-        led.plot(X, Y)
-        sprite.set(LedSpriteProperty.Y, 0)
+        game.createSprite(X, Y)
+sprite.set(LedSpriteProperty.Y, 0)
+        sprite.set(LedSpriteProperty.X, 2)
+    } else if (spielfeld[Y + 1][X] == 1) {
+        spielfeld[Y][X] = 1
+        game.createSprite(X, Y)
+sprite.set(LedSpriteProperty.Y, 0)
         sprite.set(LedSpriteProperty.X, 2)
     }
-    else if (spielfeld[Y + 1][X] == 1) {
-        spielfeld[Y][X] = 1
-        led.plot(X, Y)
-        sprite.set(LedSpriteProperty.Y, 0)
-        sprite.set(LedSpriteProperty.X, 2)
-    } 
-    	
-    
 })
